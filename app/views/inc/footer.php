ec124e7ee0534e2e7df5b414b5773501aaf059c7
dGheek <88236719+sarahdaniels09@users.noreply.github.com>
2021-11-05 08:06:21 -0700

    update
    file

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,37 +1,53 @@
-        
-            <footer class="footer text-center">
-                &copy <?php echo date('Y').' '.$core->site_name;?> - <?php echo $lang['foot'] ?>
-            </footer>
+        <div id="ytWidget"></div>
+        <script src="https://translate.yandex.net/website-widget/v1/widget.js?widgetId=ytWidget&pageLang=en&widgetTheme=light&autoMode=true" type="text/javascript"></script>
 
-            <!-- End footer -->
+        <footer class="footer text-center">
+            &copy <?php echo date('Y') . ' ' . $core->site_name; ?> - <?php echo $lang['foot'] ?>
+        </footer>
+
+        <!-- End footer -->
         </div>
-    </div>
+        </div>
 
-    <!-- End Wrapper -->
-    
+        <!-- End Wrapper -->
 
-    <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
-    <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- apps -->
-    <script src="assets/js/app.min.js"></script>
-    <script src="assets/js/app.init.js"></script>
-    <script src="assets/js/app-style-switcher.js"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-    <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-    <!--Wave Effects -->
-    <script src="assets/js/waves.js"></script>
-    <!--Menu sidebar -->
-    <script src="assets/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
-    <script src="assets/js/custom.min.js"></script>
 
-    
-    <script src="assets/extra-libs/c3/d3.min.js"></script>
-    <script src="assets/extra-libs/c3/c3.min.js"></script>
-    <!--chartjs -->
-    <script src="assets/libs/chart.js/dist/Chart.min.js"></script>
+        <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+        <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- apps -->
+        <script src="assets/js/app.min.js"></script>
+        <script src="assets/js/app.init.js"></script>
+        <script src="assets/js/app-style-switcher.js"></script>
+        <!-- slimscrollbar scrollbar JavaScript -->
+        <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+        <script src="assets/extra-libs/sparkline/sparkline.js"></script>
+        <!--Wave Effects -->
+        <script src="assets/js/waves.js"></script>
+        <!--Menu sidebar -->
+        <script src="assets/js/sidebarmenu.js"></script>
+        <!--Custom JavaScript -->
+        <script src="assets/js/custom.min.js"></script>
 
-</body>
 
-</html>
+        <script src="assets/extra-libs/c3/d3.min.js"></script>
+        <script src="assets/extra-libs/c3/c3.min.js"></script>
+        <!--chartjs -->
+        <script src="assets/libs/chart.js/dist/Chart.min.js"></script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {},
+                Tawk_LoadStart = new Date();
+            (function() {
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/6184414c6885f60a50ba5dbb/1fjm9u5n5';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
+        </body>
+
+        </html>
