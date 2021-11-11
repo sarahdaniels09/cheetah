2e33b5255c7fa49251974edbb12bd6cf4ba4dac7
dGheek <88236719+sarahdaniels09@users.noreply.github.com>
2021-11-11 06:44:08 -0800

    update
    update

--- app/views/print/print_inv_ship.php
+++ app/views/print/print_inv_ship.php
@@ -430,7 +430,6 @@ $address_order = $db->registro();
                     <table id="items">
                         <b><?php echo $receiver_data->fname . " " . $receiver_data->lname; ?></b></br> </br>
                         <?php echo $address_order->recipient_address; ?> </br>
-                        <?php echo $address_order->recipient_country . " | " . $address_order->recipient_city; ?> </br>
                         <?php echo $receiver_data->phone; ?> </br>
                         <?php echo $receiver_data->email; ?>
                     </table>
