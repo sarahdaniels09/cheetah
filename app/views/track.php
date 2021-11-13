08e6b0305501569435a6d9a6c8ab054265e2ed6a
dGheek <88236719+sarahdaniels09@users.noreply.github.com>
2021-11-13 09:30:34 -0800

    update
    update

--- app/views/track.php
+++ app/views/track.php
@@ -58,9 +58,9 @@ if ($track != null) {
 	$db->query("SELECT * FROM users where id= '" . $track->sender_id . "'");
 	$sender_data = $db->registro();
 
-	$db->query("SELECT * FROM users_multiple_addresses where id_addresses= '" . $_POST["sender_address_id"] . "'");
+	/* $db->query("SELECT * FROM users_multiple_addresses where id_addresses= '" . $_POST["sender_address_id"] . "'");
 
-	$sender_address_data = $db->registro();
+	$sender_address_data = $db->registro(); */
 
 	$db->query("SELECT * FROM users where id= '" . $track->receiver_id . "'");
 	$receiver_data = $db->registro();
@@ -415,7 +415,7 @@ if ($track != null) {
 
 											<div class="col-md-3">
 												<div class="trackstatus-title">
-													<label>Destination: <b><?php echo $sender_address->too; ?></b></label>
+													<label>Destination: <b><?php echo $sender_address->recipient_zip_code; ?></b></label>
 												</div>
 											</div>
 
