f353a261a851d2db5eb3852911ede6253ce1bc7d
dGheek <88236719+sarahdaniels09@users.noreply.github.com>
2021-11-15 00:12:03 -0800

    update
    update

--- app/views/print/print_inv_ship.php
+++ app/views/print/print_inv_ship.php
@@ -678,10 +678,11 @@ $address_order = $db->registro();
         <table id="signing">
             <tr class="noBorder">
                 <td align="center">
-                    <img src="https://cheetahmileslogistics.com/app/assets/images/cheetahsign.png" width="200px" alt="Sign">
+                    <img src="https://cheetahmileslogistics.com/app/assets/images/cheetahsign.png" width="150px" alt="Sign">
                     <h4></h4>
                 </td>
                 <td align="center">
+                    <b><?php echo $receiver_data->fname . " " . $receiver_data->lname; ?></b>
                     <h4></h4>
                 </td>
             </tr>
