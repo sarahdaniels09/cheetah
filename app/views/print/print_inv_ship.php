eacfd1f43616d4586d30d8dae29668393c9c21fb
dGheek <88236719+sarahdaniels09@users.noreply.github.com>
2021-11-14 23:13:49 -0800

    update
    update

--- app/views/print/print_inv_ship.php
+++ app/views/print/print_inv_ship.php
@@ -678,7 +678,7 @@ $address_order = $db->registro();
         <table id="signing">
             <tr class="noBorder">
                 <td align="center">
-                    <img src="../../assets/images/cheetahsign.png" alt="Sign">
+                    <img src="../../assets/images/cheetahsign.png" width="150px" alt="Sign">
                     <h4></h4>
                 </td>
                 <td align="center">
